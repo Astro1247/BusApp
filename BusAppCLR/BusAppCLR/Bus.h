853788570b1aad74acaccaedc40a39c9cfbf111a
Astro <taynik777@gmail.com>
2017-06-02 01:25:41 +0300

    v0.1.1

--- BusAppCLR/BusAppCLR/Bus.h
+++ BusAppCLR/BusAppCLR/Bus.h
@@ -3,7 +3,15 @@ ref class Bus
 {
 public:
 	int busCount;
-	Bus() { }
-	Bus(int id, int stationID, int routeID);
+	Bus();
+	void stop();
+	static void print();
+	//Bus(int id, int stationID, int routeID);
+	//Bus(int k) { busCount = k; };
+private:
+	int money;
+	int pass;
+	int pass_out;
+	int count;
 };
 
--- BusAppCLR/BusAppCLR/MainForm.cpp
+++ BusAppCLR/BusAppCLR/MainForm.cpp
@@ -5,13 +5,17 @@
 using namespace System;
 using namespace System::Windows::Forms;
 
+
+
 namespace BusAppCLR {
 	System::Void MainForm::callBusManagement(System::Object^  sender, System::EventArgs^  e) {
 		StationManagement^ f2 = gcnew StationManagement(this);
 		f2->Show();
+		//log(gcnew String("123"));
 	}
 	const int count = 10;
-	Bus bus1[count];
+	//Bus bus1[count];
+
 }
 
 
@@ -21,4 +25,12 @@ void Main(array<String^>^ args) {
 	Application::SetCompatibleTextRenderingDefault(false);
 	BusAppCLR::MainForm mainForm;
 	Application::Run(%mainForm);
+
+	
+} 
+
+void BusAppCLR::MainForm::log(System::String^ msg)
+{
+	System::Windows::Forms::Label^ label8 = gcnew System::Windows::Forms::Label();
+	label8->Text += "55555";
 }
